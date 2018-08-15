18722c94bca169ba90637817c92f65d7c10ab815
Jonas Frid <jonas.frid@bybrick.se>
2018-08-15 11:07:07 +0200

    Fix

--- AndroidLayoutTests/Code/MainActivity.cs
+++ AndroidLayoutTests/Code/MainActivity.cs
@@ -1,7 +1,10 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Support.Design.Widget;
+using Android.Support.V4.Widget;
 using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
 
 namespace AndroidLayoutTests.Code
 {
@@ -10,6 +13,8 @@ namespace AndroidLayoutTests.Code
     {
         // Private Members
         private BottomNavigationView _bottomNavigationView;
+        private LinearLayout _bottomSheetHeaderLayout;
+        private TextView _bottomSheetBodyTextView;
 
 
         // -----------------------------------------------------------------------------
@@ -27,9 +32,19 @@ namespace AndroidLayoutTests.Code
             SetSupportActionBar(FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar));
 
             // Controls
+            _bottomSheetHeaderLayout = FindViewById<LinearLayout>(Resource.Id.bottomSheetHeaderLayout);
+            _bottomSheetBodyTextView = FindViewById<TextView>(Resource.Id.bottomSheetBodyTextView);
+
             _bottomNavigationView = FindViewById<BottomNavigationView>(Resource.Id.bottom_navigation);
 
             _bottomNavigationView.NavigationItemSelected += BottomNavigation_NavigationItemSelected;
+
+            var coordinatorLayout = FindViewById<CoordinatorLayout>(Resource.Id.mainLayout);
+            var colorBottomSheet = coordinatorLayout.FindViewById<NestedScrollView>(Resource.Id.bottomSheetScrollView);
+            var bottomSheetBehavior = BottomSheetBehavior.From(colorBottomSheet);
+
+            //bottomSheetBehavior.PeekHeight = 0;
+            bottomSheetBehavior.SetBottomSheetCallback(new CustomBottomSheetCallback(_bottomSheetHeaderLayout, _bottomSheetBodyTextView));
 
             // Coordinator Layout
             //var coordinatorLayout = FindViewById<CoordinatorLayout>(Resource.Id.mainLayout);
@@ -93,6 +108,69 @@ namespace AndroidLayoutTests.Code
             SupportFragmentManager.BeginTransaction()
                                   .Replace(Resource.Id.container, fragment)
                                   .Commit();
+        }
+
+
+        // -----------------------------------------------------------------------------
+
+        // Classes
+        public class CustomBottomSheetCallback : BottomSheetBehavior.BottomSheetCallback
+        {
+            private readonly View _headerView;
+            private readonly View _bodyView;
+
+            public CustomBottomSheetCallback(View headerView, View bodyView)
+            {
+                _headerView = headerView;
+                _bodyView = bodyView;
+            }
+
+            public override void OnStateChanged(View bottomSheet, int newState)
+            {
+                if (newState == BottomSheetBehavior.StateHidden)
+                {
+                    System.Diagnostics.Debug.WriteLine("StateHidden");
+                    return;
+                }
+
+                if (newState == BottomSheetBehavior.StateDragging)
+                {
+                    System.Diagnostics.Debug.WriteLine("StateDragging");
+                    //_view.Visibility = ViewStates.Visible;
+                    return;
+                }
+
+                if (newState == BottomSheetBehavior.StateExpanded)
+                {
+                    System.Diagnostics.Debug.WriteLine("StateExpanded");
+                    //_view.Visibility = ViewStates.Gone;
+                    return;
+                }
+
+                if (newState == BottomSheetBehavior.StateSettling)
+                {
+                    System.Diagnostics.Debug.WriteLine("StateSettling");
+                    return;
+                }
+
+                if (newState == BottomSheetBehavior.StateCollapsed)
+                {
+                    System.Diagnostics.Debug.WriteLine("StateCollapsed");
+                    return;
+                }
+            }
+
+            public override void OnSlide(View bottomSheet, float slideOffset)
+            {
+                var alpha = 1 - (slideOffset * 4f);
+                if (alpha < 0)
+                {
+                    alpha = 0;
+                }
+
+                _headerView.Animate().Alpha(alpha).SetDuration(0).Start();
+                _bodyView.Animate().Alpha(slideOffset).SetDuration(0).Start();
+            }
         }
     }
 }
--- AndroidLayoutTests/Resources/Resource.designer.cs
+++ AndroidLayoutTests/Resources/Resource.designer.cs
@@ -2411,8 +2411,8 @@ namespace AndroidLayoutTests
 			// aapt resource value: 0x7f080037
 			public const int SYM = 2131230775;
 			
-			// aapt resource value: 0x7f0800a8
-			public const int action0 = 2131230888;
+			// aapt resource value: 0x7f0800aa
+			public const int action0 = 2131230890;
 			
 			// aapt resource value: 0x7f08007c
 			public const int action_bar = 2131230844;
@@ -2435,32 +2435,32 @@ namespace AndroidLayoutTests
 			// aapt resource value: 0x7f08005a
 			public const int action_bar_title = 2131230810;
 			
-			// aapt resource value: 0x7f0800a5
-			public const int action_container = 2131230885;
+			// aapt resource value: 0x7f0800a7
+			public const int action_container = 2131230887;
 			
 			// aapt resource value: 0x7f08007d
 			public const int action_context_bar = 2131230845;
 			
-			// aapt resource value: 0x7f0800ac
-			public const int action_divider = 2131230892;
-			
-			// aapt resource value: 0x7f0800bb
-			public const int action_home = 2131230907;
-			
-			// aapt resource value: 0x7f0800a6
-			public const int action_image = 2131230886;
-			
-			// aapt resource value: 0x7f0800bc
-			public const int action_item_2 = 2131230908;
+			// aapt resource value: 0x7f0800ae
+			public const int action_divider = 2131230894;
 			
 			// aapt resource value: 0x7f0800bd
-			public const int action_item_3 = 2131230909;
+			public const int action_home = 2131230909;
+			
+			// aapt resource value: 0x7f0800a8
+			public const int action_image = 2131230888;
 			
 			// aapt resource value: 0x7f0800be
-			public const int action_item_4 = 2131230910;
+			public const int action_item_2 = 2131230910;
 			
 			// aapt resource value: 0x7f0800bf
-			public const int action_item_5 = 2131230911;
+			public const int action_item_3 = 2131230911;
+			
+			// aapt resource value: 0x7f0800c0
+			public const int action_item_4 = 2131230912;
+			
+			// aapt resource value: 0x7f0800c1
+			public const int action_item_5 = 2131230913;
 			
 			// aapt resource value: 0x7f080003
 			public const int action_menu_divider = 2131230723;
@@ -2477,11 +2477,11 @@ namespace AndroidLayoutTests
 			// aapt resource value: 0x7f08005c
 			public const int action_mode_close_button = 2131230812;
 			
-			// aapt resource value: 0x7f0800a7
-			public const int action_text = 2131230887;
+			// aapt resource value: 0x7f0800a9
+			public const int action_text = 2131230889;
 			
-			// aapt resource value: 0x7f0800b5
-			public const int actions = 2131230901;
+			// aapt resource value: 0x7f0800b7
+			public const int actions = 2131230903;
 			
 			// aapt resource value: 0x7f08005d
 			public const int activity_chooser_view_content = 2131230813;
@@ -2516,6 +2516,12 @@ namespace AndroidLayoutTests
 			// aapt resource value: 0x7f08003d
 			public const int bottom = 2131230781;
 			
+			// aapt resource value: 0x7f0800a6
+			public const int bottomSheetBodyTextView = 2131230886;
+			
+			// aapt resource value: 0x7f0800a5
+			public const int bottomSheetHeaderLayout = 2131230885;
+			
 			// aapt resource value: 0x7f0800a4
 			public const int bottomSheetScrollView = 2131230884;
 			
@@ -2525,8 +2531,8 @@ namespace AndroidLayoutTests
 			// aapt resource value: 0x7f080063
 			public const int buttonPanel = 2131230819;
 			
-			// aapt resource value: 0x7f0800a9
-			public const int cancel_action = 2131230889;
+			// aapt resource value: 0x7f0800ab
+			public const int cancel_action = 2131230891;
 			
 			// aapt resource value: 0x7f080045
 			public const int center = 2131230789;
@@ -2540,8 +2546,8 @@ namespace AndroidLayoutTests
 			// aapt resource value: 0x7f080073
 			public const int checkbox = 2131230835;
 			
-			// aapt resource value: 0x7f0800b1
-			public const int chronometer = 2131230897;
+			// aapt resource value: 0x7f0800b3
+			public const int chronometer = 2131230899;
 			
 			// aapt resource value: 0x7f08004e
 			public const int clip_horizontal = 2131230798;
@@ -2603,8 +2609,8 @@ namespace AndroidLayoutTests
 			// aapt resource value: 0x7f080030
 			public const int end = 2131230768;
 			
-			// aapt resource value: 0x7f0800b7
-			public const int end_padder = 2131230903;
+			// aapt resource value: 0x7f0800b9
+			public const int end_padder = 2131230905;
 			
 			// aapt resource value: 0x7f08003f
 			public const int enterAlways = 2131230783;
@@ -2648,8 +2654,8 @@ namespace AndroidLayoutTests
 			// aapt resource value: 0x7f080062
 			public const int icon = 2131230818;
 			
-			// aapt resource value: 0x7f0800b6
-			public const int icon_group = 2131230902;
+			// aapt resource value: 0x7f0800b8
+			public const int icon_group = 2131230904;
 			
 			// aapt resource value: 0x7f08003a
 			public const int ifRoom = 2131230778;
@@ -2657,8 +2663,8 @@ namespace AndroidLayoutTests
 			// aapt resource value: 0x7f08005f
 			public const int image = 2131230815;
 			
-			// aapt resource value: 0x7f0800b2
-			public const int info = 2131230898;
+			// aapt resource value: 0x7f0800b4
+			public const int info = 2131230900;
 			
 			// aapt resource value: 0x7f080059
 			public const int italic = 2131230809;
@@ -2693,14 +2699,14 @@ namespace AndroidLayoutTests
 			// aapt resource value: 0x7f080099
 			public const int main_content = 2131230873;
 			
+			// aapt resource value: 0x7f0800bc
+			public const int masked = 2131230908;
+			
+			// aapt resource value: 0x7f0800ad
+			public const int media_actions = 2131230893;
+			
 			// aapt resource value: 0x7f0800ba
-			public const int masked = 2131230906;
-			
-			// aapt resource value: 0x7f0800ab
-			public const int media_actions = 2131230891;
-			
-			// aapt resource value: 0x7f0800b8
-			public const int message = 2131230904;
+			public const int message = 2131230906;
 			
 			// aapt resource value: 0x7f080031
 			public const int middle = 2131230769;
@@ -2723,14 +2729,14 @@ namespace AndroidLayoutTests
 			// aapt resource value: 0x7f08001e
 			public const int normal = 2131230750;
 			
-			// aapt resource value: 0x7f0800b4
-			public const int notification_background = 2131230900;
+			// aapt resource value: 0x7f0800b6
+			public const int notification_background = 2131230902;
 			
-			// aapt resource value: 0x7f0800ae
-			public const int notification_main_column = 2131230894;
+			// aapt resource value: 0x7f0800b0
+			public const int notification_main_column = 2131230896;
 			
-			// aapt resource value: 0x7f0800ad
-			public const int notification_main_column_container = 2131230893;
+			// aapt resource value: 0x7f0800af
+			public const int notification_main_column_container = 2131230895;
 			
 			// aapt resource value: 0x7f08009f
 			public const int pager = 2131230879;
@@ -2759,11 +2765,11 @@ namespace AndroidLayoutTests
 			// aapt resource value: 0x7f08004a
 			public const int right = 2131230794;
 			
-			// aapt resource value: 0x7f0800b3
-			public const int right_icon = 2131230899;
+			// aapt resource value: 0x7f0800b5
+			public const int right_icon = 2131230901;
 			
-			// aapt resource value: 0x7f0800af
-			public const int right_side = 2131230895;
+			// aapt resource value: 0x7f0800b1
+			public const int right_side = 2131230897;
 			
 			// aapt resource value: 0x7f08000c
 			public const int save_image_matrix = 2131230732;
@@ -2867,8 +2873,8 @@ namespace AndroidLayoutTests
 			// aapt resource value: 0x7f08004b
 			public const int start = 2131230795;
 			
-			// aapt resource value: 0x7f0800aa
-			public const int status_bar_latest_event_content = 2131230890;
+			// aapt resource value: 0x7f0800ac
+			public const int status_bar_latest_event_content = 2131230892;
 			
 			// aapt resource value: 0x7f080076
 			public const int submenuarrow = 2131230838;
@@ -2903,8 +2909,8 @@ namespace AndroidLayoutTests
 			// aapt resource value: 0x7f080015
 			public const int textinput_error = 2131230741;
 			
-			// aapt resource value: 0x7f0800b0
-			public const int time = 2131230896;
+			// aapt resource value: 0x7f0800b2
+			public const int time = 2131230898;
 			
 			// aapt resource value: 0x7f08001c
 			public const int title = 2131230748;
@@ -2960,8 +2966,8 @@ namespace AndroidLayoutTests
 			// aapt resource value: 0x7f080016
 			public const int view_offset_helper = 2131230742;
 			
-			// aapt resource value: 0x7f0800b9
-			public const int visible = 2131230905;
+			// aapt resource value: 0x7f0800bb
+			public const int visible = 2131230907;
 			
 			// aapt resource value: 0x7f08003c
 			public const int withText = 2131230780;
